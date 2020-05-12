cc02d3e365ded38fcf52612dabe7b9ce9ffbf84d
4ubIceK <humanidx@tut.by>
2020-05-13 00:16:21 +0300

    add db connection settings and db scripts

---
+++ src/main/resources/database/data.sql
@@ -0,0 +1,13 @@
+insert into status (name) values
+('available'),
+('sold'),
+('test drive');
+
+insert into cars (model, status_id) values
+('Mersedes', 1),
+('Audi', 2),
+('Volvo', 3),
+('Opel', 1),
+('Mersedes', 3),
+('Opel', 2),
+('Audi', 1)
